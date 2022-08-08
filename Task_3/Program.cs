37c8461803cec3ab15826e76250eb83ce40161fa
Denis <denis-shkurko@yandex.ru>
2022-08-08 16:42:15 +0700

    четное число

---
+++ Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число ");
+int numberA = int.Parse(Console.ReadLine());
+
+if (numberA%2==0)
+{
+    Console.WriteLine("Число чётное");
+}
+
+else
+{
+    Console.WriteLine("Число нечётное");
+}
