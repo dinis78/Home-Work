a8d8cca1fc18033b7e3d9a4609ae58b0076364a2
Denis <denis-shkurko@yandex.ru>
2022-08-08 15:56:11 +0700

    Tasr_2 max

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите Первое число ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Второе число. ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Третье число. ");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB>max) max = numberB;
+if (numberC>max) max = numberC;
+
+Console.Write("Максимальное число, ");
+Console.WriteLine(max);
+
+
+    
+
