538dbb1acb389f3e8fdc53a0e91cb01ae2a5c1b5
Denis <denis-shkurko@yandex.ru>
2022-08-08 18:26:18 +0700

    четные числа

---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число ");
+int numberA = int.Parse(Console.ReadLine());
+
+  for(int numberB=2; numberB<=numberA; numberB++) 
+  {
+   if(numberB%2 == 0) 
+   {
+    Console.Write("{0} ", numberB);
+   }
+  }
+  Console.ReadKey();
+  
+ 
+
