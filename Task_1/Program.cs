f4d6c3c883a03dd641023a065d9e41db3d24ecb5
Denis <denis-shkurko@yandex.ru>
2022-08-08 13:53:02 +0700

    max, min

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число. ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Второе число. ");
+int numberB = int.Parse(Console.ReadLine());
+
+
+
+if (numberA>numberB)
+
+{
+    Console.WriteLine("Первое число больше второго");
+}
+
+else
+Console.WriteLine("Второе число больше первого");
